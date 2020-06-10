a4c797ff20463a7b7a07e589f7d51d9906d14d50
Adewole Joel <joeladewole3@gmail.com>
2020-06-10 01:14:43 +0100

    Finished API

---
+++ page_status.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * PHP/cURL function to check a web site status. If HTTP status is not 200 or 302, or
+ * the requests takes longer than 10 seconds, the website is unreachable.
+ * 
+ *
+ * @param string $url URL that must be checked
+ */
+
+function url_test( $url ) {
+  $url = stripslashes(urldecode($url));
+  
+  //$outputs holds json's object
+  $outputs = array();
+
+  $timeout = 10;
+  $ch = curl_init();
+  curl_setopt ( $ch, CURLOPT_URL, $url );
+  curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+  curl_setopt ( $ch, CURLOPT_TIMEOUT, $timeout );
+
+
+  $http_respond = curl_exec($ch);
+  $http_respond = trim( strip_tags( $http_respond ) );
+  $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+
+  //Run a check on the status valid to distinguish the status comment
+  switch ($http_code) {
+
+    //For case where status is >100 bu < 199
+    case $http_code >= 100 &&  $http_code <= 199:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'Informative Response',
+        'url_status' => $http_code,
+      );
+      return json_encode($outputs);
+      break;
+
+    //For case where status is >200 bu < 299
+    case $http_code >= 200 &&  $http_code <= 299:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'successful Response',
+        'url_status' => $http_code,
+      );
+      return json_encode($outputs);
+      break;
+
+    //For case where status is >300 bu < 399
+    case $http_code >= 300 &&  $http_code <= 399:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'Redirects',
+        'url_status' => $http_code,
+      );
+      return json_encode($outputs);
+      break;
+
+    //For case where status is >400 bu < 499
+    case $http_code >= 400 &&  $http_code <= 499:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'client Error',
+        'url_status' => $http_code,
+      );
+      return json_encode($outputs);
+      break;
+
+    //For case where status is >500 bu < 599
+    case $http_code >= 500 &&  $http_code <= 599:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'Server Error',
+        'url_status' => $http_code,
+      );
+      return json_encode($outputs);
+      break;
+    
+    //For case where status is outside considered specs
+    default:
+      $outputs[$url]= array(
+        'url'       => $url,
+        'comment'   => 'Url response is invalid',
+        'url_status' => $http_code,
+      );
+      break;
+  }
+  curl_close( $ch );
+}
+
+
+//A sample run on the script function
+$website =  urlencode("https://www.google.com");
+echo (url_test($website));
+?>
---
+++ page_traffic.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * PHP/cURL function to check a web site traffic. If the website is unreachable, ERRNO is thrown.
+ * @param string $url URL that must be checked
+ */
+
+function url_test($url) {
+  $url = urldecode($url);
+  
+  //$outputs holds json's object
+  $outputs = array();
+
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if(curl_exec($ch) !== false) {
+        $info = curl_getinfo($ch);
+        $outputs[$url] = array(
+           'lookupTime' => ($info['namelookup_time'] * 1000) . 'ms',
+           'connectTime' => ($info['connect_time'] * 1000) . 'ms',
+           'pretransferTime' => ($info['pretransfer_time']) . 'ms',
+           'redirectTime' => ($info['redirect_time'] * 1000) . 'ms',
+           'startTransferTime' => ($info['starttransfer_time'] * 1000) . 'ms',
+           'totalTime' => ($info['total_time'] * 1000) . 'ms',
+        );
+        return json_encode($outputs);
+    } else {
+        echo 'Error: ' . curl_error($ch);
+    }
+  curl_close( $ch );
+}
+
+
+
+
+//A sample run on the script function
+$website =  urlencode("www.slack.com");
+echo (url_test($website));
+
+
+?>
