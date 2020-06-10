cf7db2c8347233542bed1e690a2510c8234cbc1e
wolz-CODElife <55518764+wolz-CODElife@users.noreply.github.com>
2020-06-10 04:02:02 +0100

    Update page_traffic.php

--- WebMonitor/page_traffic.php
+++ WebMonitor/page_traffic.php
@@ -33,7 +33,12 @@ function url_test($url) {
 
 
 //A sample run on the script function
-$website =  urlencode("www.slack.com");
+if (isset($_GET['url'])) {
+  $website =  urlencode($_GET['url']);
+}
+else{
+  $website =  urlencode("https://www.google.com");
+}
 echo (url_test($website));
 
 
